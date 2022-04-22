fd2b856dd534a84c902ba29b13e6335898236f52
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-04-22 23:32:22 +0700

    room db

---
+++ app/src/main/java/com/apps/roomapp/EditPengeluaranActivity.kt
@@ -0,0 +1,11 @@
+package com.apps.roomapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPengeluaranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pengeluaran)
+    }
+}
---
+++ app/src/main/java/com/apps/roomapp/room/PengeluaranDao.kt
@@ -0,0 +1,20 @@
+package com.apps.roomapp.room
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+import com.apps.roomapp.Pengeluaran
+
+@Dao
+interface PengeluaranDao {
+    @Query("SELECT * from Pengeluaran")
+    fun getAllPengeluaran(): List<Pengeluaran>
+
+    @Insert(onConflict = REPLACE)
+    fun insertPengeluaran(pengeluaran: Pengeluaran):Long
+
+    @Update
+    fun updatePengeluaran(pengeluaran: Pengeluaran):Int
+
+    @Delete
+    fun deletePengeluaran(pengeluaran: Pengeluaran):Int
+}
